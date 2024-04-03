557107921c9c0f6e2ff9df7e5b10a707b61220d5
Yain Porter <144638203+yainporter@users.noreply.github.com>
2024-04-03 14:59:01 -0700

    feat: Complete US-1

--- spec/features/markets/index_spec.rb
+++ spec/features/markets/index_spec.rb
@@ -16,8 +16,26 @@ RSpec.describe "Markets Index Page", type: :feature do
         expect(page).to have_selector("th", text: "State")
         expect(page).to have_selector("th", text: "")
         expect(page).to have_selector("td", text: "More Info")
+
         all("tr").each do |row|
           expect(row.text).not_to be_empty
+        end
+      end
+    end
+
+    it "has a button to more info for a market", :vcr do
+      within "#markets-list" do
+        expect(page).to have_selector("th", text: "Name")
+        expect(page).to have_selector("th", text: "City")
+        expect(page).to have_selector("th", text: "State")
+        expect(page).to have_selector("th", text: "")
+        expect(page).to have_selector("td", text: "More Info")
+
+        all("tr").each do |row|
+          if row.has_button?
+            row.click_button("More Info")
+            expect(page.current_path).to_not eq(markets_path)
+          end
         end
       end
     end
